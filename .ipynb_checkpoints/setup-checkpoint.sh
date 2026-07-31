18efe5e47969c6284865f48aadb761fca1169158
vortexdigitalera <rabiuhadisalisu@icloud.com>
2026-07-31 21:50:56 +0000

    good

---
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -0,0 +1,31 @@
+sudo apt-get update -qq
+sudo apt-get install -y -qq \
+  libatk1.0-0 \
+  libatk-bridge2.0-0 \
+  libcups2 \
+  libdrm2 \
+  libdbus-1-3 \
+  libxkbcommon0 \
+  libxcomposite1 \
+  libxdamage1 \
+  libxrandr2 \
+  libgbm1 \
+  libpango-1.0-0 \
+  libcairo2 \
+  libnspr4 \
+  libnss3 \
+  libxfixes3 \
+  libasound2t64 \
+  libx11-xcb1 \
+  libxcb1 \
+  libxext6 \
+  libxrender1 \
+  libxtst6 \
+  libxi6 \
+  libxss1 \
+  fonts-noto-color-emoji \
+  fonts-freefont-ttf \
+  fonts-unifont \
+  fonts-ipafont-gothic \
+  fonts-wqy-zenhei \
+  fonts-tlwg-loma-otf
